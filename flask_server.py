172112141ba58bfa44b373dbdfdcbed3fdd61aee
NikolayMal <55692364+NikolayMal@users.noreply.github.com>
2022-08-18 20:21:10 +0100

    Settings Form
    Beginning of settings form.

--- flask_server.py
+++ flask_server.py
@@ -4,6 +4,7 @@ from flask_uploads import UploadSet, IMAGES, configure_uploads
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms import SubmitField
+from PIL import Image
 
 app = Flask(__name__)
 
