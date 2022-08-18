3db5cc559aa332aec3d055f186d8b73143a12ad2
NikolayMal <55692364+NikolayMal@users.noreply.github.com>
2022-08-18 17:44:29 +0100

    Re-done image uploading
    Now uploads all in one page, seamless showing of image.

--- flask_server.py
+++ flask_server.py
@@ -1,49 +1,50 @@
-from flask import Flask, render_template, request, session
-from imageProcessing import imageProcessing
+from flask import Flask, render_template, request, send_from_directory, url_for
 import os
-from werkzeug.utils import secure_filename
-
-
-
+from flask_uploads import UploadSet, IMAGES, configure_uploads
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import SubmitField
 
 app = Flask(__name__)
 
 UPLOAD_FOLDER = os.path.join('static', 'uploads')
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg'}
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOADED_PHOTOS_DEST'] = UPLOAD_FOLDER
+
+photos = UploadSet('photos', IMAGES) # Images = set of extensions of image files
+configure_uploads(app, photos)
 
 app.secret_key = 'This is your secret key to utilize session in Flask'
 
-@app.route('/flask', methods=['GET'])
-def index():
-    img = "data/letter.png"
-    imageProc = imageProcessing(img)
-    img = imageProc.run(img)
-    print(img)
-    return render_template('index.html', user_image = img)
+
+class UploadForm(FlaskForm):
+    photo = FileField(
+        validators=[
+            FileAllowed(photos, 'Images only!'),
+            FileRequired('File was empty!')
+        ]
+    )
+    submit = SubmitField('Upload')
+
+
+@app.route('/uploads/<filename>')
+def get_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
 
 @app.route('/',  methods=("POST", "GET"))
 def uploadFile():
-    if request.method == 'POST':
-        # Upload file flask
-        uploaded_img = request.files['uploaded-file']
-        # Extracting uploaded data file name
-        img_filename = secure_filename(uploaded_img.filename)
-        # Upload file to database (defined uploaded folder in static path)
-        uploaded_img.save(os.path.join(app.config['UPLOAD_FOLDER'], img_filename))
-        # Storing uploaded file path in flask session
-        session['uploaded_img_file_path'] = os.path.join(app.config['UPLOAD_FOLDER'], img_filename)
+    # if request.method == 'POST':
+        form = UploadForm()
+        if form.validate_on_submit():
+            filename = photos.save(form.photo.data)
+            file_url = url_for('get_file', filename=filename)
+        else:
+            file_url = None
 
-        return render_template('imageupload.html')
- 
-    return render_template('index.html')
+        return render_template('index.html', form=form, file_url=file_url)
 
-@app.route('/imageshow')
-def displayImage():
-    # Retrieving uploaded file path from session
-    img_file_path = session.get('uploaded_img_file_path', None)
-    # Display image in Flask application web page
-    return render_template('index.html', user_image = img_file_path)
 
 if __name__ == "__main__":
     app.run(port=3000, debug=True)
