42e9a665e975b80839a4b57a47c7701c7c66ffef
Meapy <dkrasovskijj@gmail.com>
2022-08-19 14:27:02 +0100

    removed useless files

--- routes/users.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-const router = express.Router();
-const joi = require("@hapi/joi");
-const models = require("../models/users");
-
-
-module.exports = router;
